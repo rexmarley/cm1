4bc2dd7cde92d077fae8985dddb8a1a5711df46f
Rex <u1173961@unimail.hud.ac.uk>
2015-03-02 22:42:32 +0000

    php move validation; TODO: translate to js

--- ChessMate/src/CM/InterfaceBundle/Controller/AjaxController.php
+++ ChessMate/src/CM/InterfaceBundle/Controller/AjaxController.php
@@ -11,25 +11,64 @@ class AjaxController extends Controller
 {    
     public function validateMoveAction(Request $request)
     {
-    	//var_dump($request->getContent()); 
+    	//TODO: all in js and just Ajax move if valid!!!!!!!!!!!!!!
     	$pieceType = $request->request->get('pieceType');
+    	$colour = $request->request->get('colour');
     	$fromSquare = $request->request->get('fromSquare');
     	$toSquare = $request->request->get('toSquare');
     	//check move
-    	$success = $this->checkMove($pieceType, $fromSquare, $toSquare);
+    	$success = $this->checkMove($pieceType, $colour, $fromSquare, $toSquare);
     	//set response
-    	$response = array("success" => $success, "pt" => $pieceType);
+    	$response = array("success" => $success, "from" => $fromSquare, "to" => $toSquare);
     	//return JSON response
     	//--return new Response(json_encode($response));
     	return new JsonResponse($response);
     }
     
-    public function checkMove($pieceType, $fromSquare, $toSquare) {
-    	//temp - move to service?
+    public function checkMove($pieceType, $colour, $fromSquare, $toSquare) {
+    	//TODO: all in js and just Ajax move if valid!!!!!!!!!!!!!!
     	$valid = false;
+		$from = explode('_', $fromSquare);
+		$fLetter = $from[0];
+		$fNumber = $from[1];
+		$to = explode('_', $toSquare);
+		$tLetter = $to[0];
+		$tNumber = $to[1];
+		//set positioning of letters TODO: something better (ASCII ?)
+		$pos = array('a' => 1,'b' => 2,'c' => 3,'d' => 4,'e' => 5,'f' => 6,'g' => 7,'h' => 8);
     	if ($pieceType == 'pawn') {
-    		$valid = true;
-    	}
+			if ($fNumber == 2 || $fNumber == 7) {
+				//allow initial movement of 2 spaces
+				//no need to worry about moving extra space: if direction is different end of board is reached
+				$colour *= 2;
+			}
+			//if ($colour == 'white') {
+			if ($tNumber > $fNumber && $fLetter == $tLetter && $tNumber - $colour <= $fNumber) {
+				$valid = true;
+			}
+			//}
+    	} elseif ($pieceType == 'rook') {
+			if ($fLetter == $tLetter || $fNumber == $tNumber) {
+				$valid = true;
+			}		
+		} elseif ($pieceType == 'knight') {
+			if ((($tNumber - $fNumber)*($tNumber - $fNumber)) + (($pos[$tLetter] - $pos[$fLetter])*($pos[$tLetter] - $pos[$fLetter])) == 5) {
+				$valid = true;
+			}		
+		} elseif ($pieceType == 'bishop') {
+			if ($tNumber - $fNumber == $pos[$tLetter] - $pos[$fLetter]) {
+				$valid = true;
+			}
+		} elseif ($pieceType == 'queen') {
+			if ($tNumber - $fNumber <= 1 && $pos[$tLetter] - $pos[$fLetter] <= 1) {
+				$valid = true;
+			}		
+		} elseif ($pieceType == 'king') {
+			if ($tNumber - $fNumber <= 1 && $pos[$tLetter] - $pos[$fLetter] <= 1) {
+				$valid = true;
+			}		
+		} 
+
     	return $valid;
     } 
 }
--- ChessMate/src/CM/InterfaceBundle/Resources/public/js/board.js
+++ ChessMate/src/CM/InterfaceBundle/Resources/public/js/board.js
@@ -12,6 +12,8 @@ $(document).ready( function() {
 	function validateMove(event, ui) {
 		var data = {
 			   	        pieceType: 'pawn',
+						//colour: 'white',
+						colour: 1, //or -1 for black
 				        fromSquare: 1,
 				        toSquare: 2,
 			        };
@@ -26,7 +28,8 @@ $(document).ready( function() {
 	        },
 	        success : function(data) {
 				//centre piece
-				$(this).append(ui.draggable.css('position','static')); //prevents visible re-move?
+				$(this).append(ui.draggable.css('position','static')); //prevents visible re-move? 
+				//TODO: disable on success
 	        }
 	    });
 	}
