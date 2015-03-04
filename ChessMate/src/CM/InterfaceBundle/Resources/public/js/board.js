6935f61483348e2d11793ca65735beca95c9ca2d
Rex <u1173961@unimail.hud.ac.uk>
2015-03-04 22:45:29 +0000

    fixed backwards king movement & added long-castle validation

--- ChessMate/src/CM/InterfaceBundle/Resources/public/js/board.js
+++ ChessMate/src/CM/InterfaceBundle/Resources/public/js/board.js
@@ -17,6 +17,12 @@ $(document).ready( function() {
 	
 	function validateMove(event, ui) {
 		var valid = false;
+		var unmoved = false;		
+		if (ui.draggable.hasClass('unmoved')) {
+			unmoved = true;
+		}
+		//console.log(unmoved);
+		//console.log(ui.draggable);
 		var fromSquare = ui.draggable.parent().attr('id');
 		var toSquare = this.id;
 		var from = fromSquare.split('_');
@@ -27,18 +33,18 @@ $(document).ready( function() {
 		var tNumber = to[1];
 		//get piece type & colour
 		var piece = ui.draggable.attr('id').split('_');
+		var colour = piece[0];
 		var pieceType = piece[1];
-		//set positioning of letters TODO: something better (ASCII ?)
+		//set positioning of letters
 		var pos = {'a': 1,'b': 2,'c': 3,'d': 4,'e': 5,'f': 6,'g': 7,'h': 8};
     	if (pieceType == 'pawn') {
     		var spaces = 1;
-			//allow initial movement of 2 spaces
-			if (fNumber == 2 || fNumber == 7) {
-				//no need to worry about moving extra space; if direction is different, end of board is reached
+			//if (fNumber == 2 || fNumber == 7) {
+			if (unmoved) {
+				//allow initial movement of 2 spaces
 				spaces = 2;
 			}
 			if (fLetter == tLetter) {
-				var colour = piece[0];
 	    		if ((colour == 'w' && tNumber > fNumber && tNumber - fNumber <= spaces)
 	    			|| (colour == 'b' && tNumber < fNumber && fNumber - tNumber <= spaces)) {
 					valid = true;
@@ -61,15 +67,30 @@ $(document).ready( function() {
 				valid = true;
 			}		
 		} else if (pieceType == 'king') {
-			if (tNumber - fNumber <= 1 && pos[tLetter] - pos[fLetter] <= 1) {
+			if (Math.abs(tNumber - fNumber) <= 1 && Math.abs(pos[tLetter] - pos[fLetter]) <= 1) {
 				valid = true;
-			}		
+			} else if (unmoved && tNumber == fNumber) {
+				if (tLetter == 'g' && $('#'+colour+'_rook_2').hasClass('unmoved') 
+					&& !$.trim($('#f_'+tNumber).text()).length && !$.trim($('#g_'+tNumber).text()).length) {
+					//allow short castle
+					valid = true;
+					//move castle
+					$('#f_'+tNumber).append($('#'+colour+'_rook_2'));
+				} else if (tLetter == 'c' && $('#'+colour+'_rook_1').hasClass('unmoved') && !$.trim($('#b_'+tNumber).text()).length 
+					&& !$.trim($('#c_'+tNumber).text()).length && !$.trim($('#d_'+ tNumber).text()).length) {
+					//allow long castle
+					valid = true;
+					//move castle
+					$('#d_'+tNumber).append($('#'+colour+'_rook_1'));
+				}
+			}
 		}
 
     	if(!valid) {
     		//invalidate move
     		ui.draggable.addClass('invalid');
     	} else {
+			ui.draggable.removeClass('unmoved');
     		//center (TODO disable board?)
     		$(this).append(ui.draggable.css('position','static'));
     	}
