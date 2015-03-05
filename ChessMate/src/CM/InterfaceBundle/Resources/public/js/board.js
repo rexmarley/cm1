481dae2f087148c0ed1b0dd95aa4698eeff528b0
Rex <u1173961@unimail.hud.ac.uk>
2015-03-05 19:29:08 +0000

    added diagonalBlocked() function. TODO: refactor

--- ChessMate/src/CM/InterfaceBundle/Resources/public/js/board.js
+++ ChessMate/src/CM/InterfaceBundle/Resources/public/js/board.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+	//make pieces draggable
 	$('.piece').draggable({
         containment : '#board',
         revert: function() {
@@ -10,11 +11,19 @@ $(document).ready( function() {
         }
 	});
 	
+	//make squares droppable
 	$('.square').droppable({
 		accept: '.piece',
 		drop: validateMove,
     });
 	
+	//set positioning of letters
+	posOf = {'a': 1,'b': 2,'c': 3,'d': 4,'e': 5,'f': 6,'g': 7,'h': 8};
+	letterAt = ['a','b','c','d','e','f','g','h'];
+	
+	/**
+	 * Validate chess move
+	 */
 	function validateMove(event, ui) {
 		var valid = false;
 		//get moved piece
@@ -31,19 +40,17 @@ $(document).ready( function() {
 		}
 		var to = toSquare.split('_');
 		var tLetter = to[0];
-		var tNumber = to[1];
+		var tNumber = parseInt(to[1], '10');
 		//get from square
 		var fromSquare = ui.draggable.parent().attr('id');
 		var from = fromSquare.split('_');
 		var fLetter = from[0];
-		var fNumber = from[1];
+		var fNumber = parseInt(from[1], '10');
 		//check if piece's first move
 		var unmoved = false;		
 		if (ui.draggable.hasClass('unmoved')) {
 			unmoved = true;
 		}
-		//set positioning of letters - TODO global?
-		var pos = {'a': 1,'b': 2,'c': 3,'d': 4,'e': 5,'f': 6,'g': 7,'h': 8};
 		//validate move
     	if (pieceType == 'pawn') {
     		var spaces = 1;
@@ -52,16 +59,16 @@ $(document).ready( function() {
 				spaces = 2;
 			}
 			if (vacant(toSquare)) {
+				//allow moving forward
 				if (fLetter == tLetter) {
 		    		if ((colour == 'w' && tNumber > fNumber && tNumber - fNumber <= spaces)
 		    			|| (colour == 'b' && tNumber < fNumber && fNumber - tNumber <= spaces)) {
 						valid = true;
 		    		}
 				}				
-			} else if (onDiagonal(tNumber, fNumber, pos[tLetter], pos[fLetter]) 
-				&& ((colour == 'w' && tNumber > fNumber) || colour == 'b' && tNumber < fNumber))  {
+			} else if (onDiagonal(tNumber, fNumber, posOf[tLetter], posOf[fLetter]) 
+				&& ((colour == 'w' && tNumber - 1 == fNumber) || colour == 'b' && fNumber - 1 == tNumber))  {
 				//occupied by own already checked --> allow take
-				//TODO: sort out rat's nest 
 				valid = true;
 				//remove taken piece and move to side
 				var taken = getOccupant(toSquare);
@@ -73,19 +80,20 @@ $(document).ready( function() {
 				valid = true;
 			}		
 		} else if (pieceType == 'knight') {
-			if (((tNumber - fNumber)*(tNumber - fNumber)) + ((pos[tLetter] - pos[fLetter])*(pos[tLetter] - pos[fLetter])) == 5) {
+			if (((tNumber - fNumber)*(tNumber - fNumber)) + ((posOf[tLetter] - posOf[fLetter])*(posOf[tLetter] - posOf[fLetter])) == 5) {
 				valid = true;
 			}		
 		} else if (pieceType == 'bishop') {
-			if (onDiagonal(tNumber, fNumber, pos[tLetter], pos[fLetter])) {
+			if (onDiagonal(tNumber, fNumber, posOf[tLetter], posOf[fLetter])) {
 				valid = true;
 			}
 		} else if (pieceType == 'queen') {
-			if (fLetter == tLetter || fNumber == tNumber || onDiagonal(tNumber, fNumber, pos[tLetter], pos[fLetter])) {
+			if (fLetter == tLetter || fNumber == tNumber 
+				|| (onDiagonal(tNumber, fNumber, posOf[tLetter], posOf[fLetter]) && !diagonalBlocked(fNumber, tNumber, fLetter, tLetter))) {
 				valid = true;
 			}		
 		} else if (pieceType == 'king') {
-			if (Math.abs(tNumber - fNumber) <= 1 && Math.abs(pos[tLetter] - pos[fLetter]) <= 1) {
+			if (Math.abs(tNumber - fNumber) <= 1 && Math.abs(posOf[tLetter] - posOf[fLetter]) <= 1) {
 				valid = true;
 			} else if (unmoved && tNumber == fNumber) {
 				if (tLetter == 'g' && $('#'+colour+'_rook_2').hasClass('unmoved') 
@@ -132,8 +140,8 @@ $(document).ready( function() {
 	/**
 	 * Check if target square is unoccupied
 	 */
-	function vacant(squareID) {		
-		return !$.trim($('#'+squareID).text()).length;
+	function vacant(squareID) {
+		return $('div#'+squareID+' div.piece').length < 1;
 	}
 	
 	/**
@@ -154,7 +162,58 @@ $(document).ready( function() {
 		return false;
 	}
 	
-	function diagonalNotBlocked(fNumber, fLetter, tNumber, tLetter) {
-		//Math.abs(tNumber - fNumber) == Math.abs(pos[tLetter] - pos[fLetter])
+	function diagonalBlocked(fNumber, tNumber, fLetter, tLetter) {
+		var fLetterPos = posOf[fLetter] - 1;
+		var tLetterPos = posOf[tLetter] - 1;
+		var range = Math.abs(tNumber - fNumber);
+		if (fLetterPos > tLetterPos) {
+			//moving left
+			if (fNumber < tNumber) {
+				//moving up
+				for (var i = 1; i < range; i++) {
+					fNumber++;
+					fLetterPos--;
+					console.log(letterAt[fLetterPos]+'_'+ fNumber);
+					if(!vacant(letterAt[fLetterPos]+'_'+ fNumber)) {
+						return true;
+					}
+				}				
+			} else {
+				//moving down
+				for (var i = 1; i < range; i++) {
+					fNumber--;
+					fLetterPos--;
+					console.log(letterAt[fLetterPos]+'_'+ fNumber);
+					if(!vacant(letterAt[fLetterPos]+'_'+ fNumber)) {
+						return true;
+					}
+				}
+			}			
+		} else {
+			//moving right
+			if (fNumber < tNumber) {
+				//moving up
+				for (var i = 1; i < range; i++) {
+					fNumber++;
+					fLetterPos++;
+					console.log(letterAt[fLetterPos]+'_'+ fNumber);
+					if(!vacant(letterAt[fLetterPos]+'_'+ fNumber)) {
+						return true;
+					}
+				}				
+			} else {
+				//moving down
+				for (var i = 1; i < range; i++) {
+					fNumber--;
+					fLetterPos++;
+					console.log(letterAt[fLetterPos]+'_'+ fNumber);
+					if(!vacant(letterAt[fLetterPos]+'_'+ fNumber)) {
+						return true;
+					}
+				}
+			}	
+		}
+
+		return false;
 	}
 });
